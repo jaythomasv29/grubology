54d0050748cbc8141f05e9bdd20e3b7c78e5e251
jaythomasv29 <91anandraj@gmail.com>
2019-10-24 15:00:26 -0400

    added project paths and components and navbar

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+
+//component decorator function
+@Component({
+    selector: 'app-header',
+    templateUrl: './header.component.html'
+})
+        export class HeaderComponent {
+}
---
+++ src/app/recipes/recipes.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+//Component decorator function
+@Component({
+    selector: 'app-recipes',
+    templateUrl: './recipes.component.html'
+})
+
+export class RecipesComponent {
+
+}
